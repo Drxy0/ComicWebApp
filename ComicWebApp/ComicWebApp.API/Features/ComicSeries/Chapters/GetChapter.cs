df93e7ebf9fd7dac462cf29d6c39e2bb26a1bbab
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2025-05-16 12:17:10 +0200

    Create GetChapter response Dto

--- ComicWebApp/ComicWebApp.API/Features/ComicSeries/Chapters/GetChapter.cs
+++ ComicWebApp/ComicWebApp.API/Features/ComicSeries/Chapters/GetChapter.cs
@@ -3,11 +3,14 @@ using ComicWebApp.API.Features.ComicSeries.ComicSeriesModels;
 using ComicWebApp.API.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using static ComicWebApp.API.Features.ComicSeries.Chapters.GetChapter;
 
 namespace ComicWebApp.API.Features.ComicSeries.Chapters;
 
 public class GetChapter
 {
+    public record GetChapterFilesResponse(Guid Id, int PageNumber);
+    public record GetChapterResponse(string? Title, float Number, Guid Id, Guid SeriesId, List<GetChapterFilesResponse> Pages);
     public class Endpnoint : IEndpoint
     {
         public void MapEndpoint(IEndpointRouteBuilder app)
@@ -28,6 +31,20 @@ public class GetChapter
             return Results.NotFound($"Chapter with Id {id} not found");
         }
 
-        return Results.Ok(chapter);
+        GetChapterResponse response = new GetChapterResponse(
+            chapter.Title,
+            chapter.Number,
+            chapter.Id,
+            chapter.SeriesId,
+            chapter.Pages
+                .OrderBy(p => p.PageNumber)
+                .Select(p => new GetChapterFilesResponse(
+                    Id: p.Id,
+                    PageNumber: p.PageNumber
+                ))
+                .ToList()
+        );
+
+        return Results.Ok(response);
     }
 }
