6157337df304e18f56935a6917472a4d8252f5f6
B. Drljević <87530056+Drxy0@users.noreply.github.com>
2025-06-12 20:58:30 +0200

    Add currentPage update on UI in reader

--- frontend/src/app/comic-series/reader/reader.component.ts
+++ frontend/src/app/comic-series/reader/reader.component.ts
@@ -1,36 +1,51 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, effect, ElementRef, HostListener, signal, viewChild, ViewChild } from '@angular/core';
 import { ComicService } from '../../services/comic.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Location } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-reader',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './reader.component.html',
   styleUrl: './reader.component.scss',
 })
 export class ReaderComponent {
   @ViewChild('page') page!: ElementRef<HTMLImageElement>;
+  currentPageSpan = viewChild<ElementRef>('currentPageSpan');
+
   private imgWidth: number = 0;
   pageUrl: SafeUrl | null = null;
-  pageCount: number = 0;
+  pageCount = signal(0); // for navigation
+  currentPage = signal(0);
 
   constructor(
     private comicService: ComicService,
-    private route: ActivatedRoute,
+    public route: ActivatedRoute,
     private router: Router,
-    private location: Location,
     private sanitiser: DomSanitizer
   ) {
-    const { chapterId, pageNumber } = this.route.snapshot.params;
-
+    const { chapterId, pageNumber } = this.route.snapshot.params
+    
+    this.route.params.subscribe(params => {
+      this.currentPage.set(+params['pageNumber']);
+    });;
+    
     this.comicService.getPage(chapterId, pageNumber).subscribe((blob: Blob) => {
       this.displayPage(blob);
     });
 
     this.comicService.getPageCount(chapterId).subscribe((response) => {
-      this.pageCount = (response as { pageCount: number }).pageCount;
+      this.pageCount.set((response as {pageCount: number}).pageCount);
+    });
+
+    effect(() => {
+      const span = this.currentPageSpan();
+      if (span) {
+        span.nativeElement.textContent = 
+          `${this.currentPage()} / ${this.pageCount()}`;
+      }
     });
   }
 
@@ -67,14 +82,14 @@ export class ReaderComponent {
 
   goToPrevPage() {
     const { chapterId, pageNumber } = this.route.snapshot.params;
-    const currentPage = +pageNumber;
+    const currentPage = pageNumber;
     const nextPage = currentPage - 1;
 
     if (nextPage <= 0 ) return;
 
     this.comicService.getPage(chapterId, nextPage).subscribe((blob: Blob) => {
       this.displayPage(blob);
-
+      this.currentPage.set(nextPage);
       this.router.navigate(['../', nextPage], {
         relativeTo: this.route,
         replaceUrl: true,
@@ -89,11 +104,11 @@ export class ReaderComponent {
     const currentPage = +pageNumber;
     const nextPage = currentPage + 1;
 
-    if (nextPage > this.pageCount) return;
+    if (nextPage > this.pageCount()) return;
 
     this.comicService.getPage(chapterId, nextPage).subscribe((blob: Blob) => {
       this.displayPage(blob);
-
+      this.currentPage.set(nextPage);
       this.router.navigate(['../', nextPage], {
         relativeTo: this.route,
         replaceUrl: true,
